b10cc08f657539c6e38410f25219009cefdc64ac
Mauro Scomparin <scompo@gmail.com>
2014-04-30 00:09:04 +0200

    added stuff
    Signed-off-by: Mauro Scomparin <scompo@gmail.com>

--- src/main/java/org/scompo/testspring/controllers/MainController.java
+++ src/main/java/org/scompo/testspring/controllers/MainController.java
@@ -1,17 +1,24 @@
 package org.scompo.testspring.controllers;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/home")
 public class MainController{
 
+	private static final String GREET_STRING = "Hello %s!";
+
+	/**
+	 * @param name
+	 * @return
+	 */
 	@ResponseBody
-	@RequestMapping(value = "/greet")	
-	public String greeter(){
-		return "Hello World!";
+	@RequestMapping(value = "/greet", method = RequestMethod.GET)
+	public String greeterByName(@RequestParam String name){
+		return String.format(GREET_STRING, name);
 	}
 }
